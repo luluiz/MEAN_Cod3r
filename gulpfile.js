af688d66afc26888b06ba66c705a367aeebe012b
luluiz <luluizbezerra@gmail.com>
2017-05-17 19:52:42 -0300

    Commit para execução de um único projeto backend + frontend.. no mesmo package.json
    Configurado o GULP para essa execução.

--- gulpfile.js
+++ gulpfile.js
@@ -4,37 +4,23 @@ const gulp = require('gulp');
 //importando o modulo gulp-util
 const util = require('gulp-util');
 
-const runSequence = require('run-sequence');
-
-const gulpLoadPlugins = require('gulp-load-plugins');
-const plugins = gulpLoadPlugins({
-  rename: {
-    'gulp-angular-templatecache': 'templateCache'
-  }
-});
-
 const _ = require('lodash');
 
 //importando as gulpTasks criadas no modulo (arquivo) deps.js
-require('./angular1/gulpTasks/app');
-require('./angular1/gulpTasks/deps');
-require('./angular1/gulpTasks/server');
+require('./gulpTasks/app');
+require('./gulpTasks/deps');
+require('./gulpTasks/server');
 
-gulp.task('nodemon', function() {
-  return plugins.nodemon({
-    script: 'loader.js',
-    nodeArgs: ['--debug'],
-    ext: 'js, html',
-    watch: _.union(defaultAssets.server.views, defaultAssets.server.allJS, defaultAssets.server.config)
-  });
+gulp.task('backend', function() {
+  require('./backend/loader');
 });
 
 //declaração da task padrão sempre que for executado o comando gulp no console
 gulp.task('default', function() {
   if (util.env.production) { //se a chamada do gulp conter a flag production
-    gulp.start('deps', 'app'); //chama duas tasks: deps e app
+    gulp.start('deps', 'app', 'backend'); //chama duas tasks: deps e app
   } else { //se for chamada para o desenvolvimento
-    // gulp.start('deps', 'app', 'server'); //iniciar as tres tasks: deps, app e server
-    runSequence('deps', 'app', 'server', 'nodemon', done);
+    gulp.start('deps', 'app', 'server', 'backend'); //iniciar as tres tasks: deps, app e server
+    // runSequence('deps', 'app', 'server', 'nodemon', done);
   }
 });
